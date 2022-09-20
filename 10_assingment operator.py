7d43780a37d75bf1499b76c98c53c572bddae364
unknown <kunalsri56987@gmail.com>
2022-09-20 18:04:46 +0530

    assingment operator

---
+++ 10_assingment operator.py
@@ -0,0 +1,46 @@
+#assing two variables to the same value
+a=b=30
+print(a,b)
+
+#assing many variables to same value
+a=b=c=d=e=f=50
+print(a,b,c,d,e,f)
+
+#assing many value by unsing ','
+a,b,c,d=10,20,30,40
+print(a,b,c,d)
+
+#Ex-1
+x=50
+x+=20 #x=x+20
+print(x)
+
+#Ex-2
+x=20
+x-=30 #x=x-30
+print(x)
+
+#Ex-3
+x=2
+x*=4 #x=x*4
+print(x)
+
+#Ex-4
+x=10
+x/=2 #x=x/2
+print(x)
+
+#Ex-5
+x=30
+x%=5 #x=x%5 it will return reminder so zero
+print(x)
+
+#Ex-6
+x=20
+x**=2 #x=x**2
+print(x)
+
+#Ex-7
+x=1000
+x//=3 #x=x//3
+print(x)
