937aa9731eeafb3ed3b6170464ace8aec30b8170
unknown <kunalsri56987@gmail.com>
2022-10-02 03:33:35 +0530

    program on del and none

---
+++ 34_del and none.py
@@ -0,0 +1,33 @@
+#del-- it delete the memory
+#Ex-1
+a=100
+print(a)
+# del a
+# print(a) #delete a so we will get name error bcz it deletes the value of a
+
+#Ex-2
+x='kunal'
+print(x)
+# del s
+# print(s)
+
+#Ex-3
+x='kunal'
+print(x)
+# del s[0]
+# print(s) #can't delete
+
+#Ex-4
+l=[10,20,30,40,50]
+print(l)
+del l[2]
+print(l) #we can delete from list
+
+#none
+#Ex-5
+a="Kutti"
+print(a)
+a=None
+print(a)
+a="cat"
+print(a)
